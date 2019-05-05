68a25b4a602e8ebf4967d7c4592b585826eff4b7
Eric Lorenzen <ericlorenzen@Erics-MacBook-Air.local>
2019-05-05 11:30:46 -0600

    Revert "Load Reddit 'hot' list when 'hot' button clicked in second nav dropdown"
    This reverts commit 3a975929b52be5735ec8d24eca395e2eb8736a1c.

--- assets/js/app.js
+++ assets/js/app.js
@@ -76,40 +76,6 @@ $('.filter_hot').click(function(){
     })
 });
 
-$('.filter_new').click(function(){
-    $.ajax('https://www.reddit.com/new/.json?limit=1', {
-        success: data => {
-            let post_list = data.data.children;
-            console.log("Successfully loaded 'new' list");
-            for(let i = 0; i < post_list.length; i++){
-                let post = post_list[i].data;
-                let votes = post.ups;
-                let title = post.title;
-                let subredditName = post.subreddit_name_prefixed;
-                let username = post.author;
-                let commentCount = post.num_comments;
-                //Add post title 
-                $('.post_title').text(title);
-                //Add subreddit name
-                $('.subreddit_name').text(subredditName);
-                //Add username
-                $('.post_author').text(username);
-                //Add number of comments
-                $('.post_comments').text(commentCount);
-                
-                if(votes === 0){
-                    $('post_title').text('•');
-                } else {
-                    $('.post_votes').text(votes);
-                }
-            }
-        }, 
-        error: function(){
-            console.log("Problem loading 'new' list");
-        }
-    })
-});
-
 
 
 
