6fb248183af0f46ef7a66f4b4c0babfc3e8fcea4
Eric Lorenzen <ericlorenzen@Erics-MacBook-Air.local>
2019-05-05 11:19:05 -0600

    Load Reddit 'hot' list when 'hot' button clicked in second nav dropdown

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
     $.ajax("https://www.reddit.com/best/.json?limit=1", {
         success: data => {
             let post_list = data.data.children;
-            console.log('Successfully loaded list');
+            console.log("Successfully loaded 'best' list");
             for(let i = 0; i < post_list.length; i++){
                 let post = post_list[i].data;
                 let votes = post.ups;
@@ -21,23 +21,61 @@ $(document).ready(function(){
                 let username = post.author;
                 let commentCount = post.num_comments;
                 //Add post title 
-                $('.post_title').append(title);
+                $('.post_title').text(title);
                 //Add subreddit name
-                $('.subreddit_name').append(subredditName);
+                $('.subreddit_name').text(subredditName);
                 //Add username
-                $('.post_author').append(username);
+                $('.post_author').text(username);
                 //Add number of comments
-                $('.post_comments').append(commentCount);
+                $('.post_comments').text(commentCount);
                 
                 if(votes === 0){
-                    $('post_title').append('•');
+                    $('post_title').text('•');
                 } else {
-                    $('.post_votes').append(votes);
+                    $('.post_votes').text(votes);
                 }
             }
         },
         error: function(){
-            console.log('Problem loading data');
+            console.log("Problem loading 'best' list");
         } 
     });
-})
+});
+
+$('.filter_hot').click(function(){
+    $.ajax('https://www.reddit.com/hot/.json?limit=1', {
+        success: data => {
+            let post_list = data.data.children;
+            console.log("Successfully loaded 'hot' list");
+            for(let i = 0; i < post_list.length; i++){
+                let post = post_list[i].data;
+                let votes = post.ups;
+                let title = post.title;
+                let subredditName = post.subreddit_name_prefixed;
+                let username = post.author;
+                let commentCount = post.num_comments;
+                //Add post title 
+                $('.post_title').text(title);
+                //Add subreddit name
+                $('.subreddit_name').text(subredditName);
+                //Add username
+                $('.post_author').text(username);
+                //Add number of comments
+                $('.post_comments').text(commentCount);
+                
+                if(votes === 0){
+                    $('post_title').text('•');
+                } else {
+                    $('.post_votes').text(votes);
+                }
+            }
+        }, 
+        error: function(){
+            console.log("Problem loading 'hot' list");
+        }
+    })
+});
+
+
+
+
