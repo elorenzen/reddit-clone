8cb0f5bff13354f6a070296d72f3d39e91ba3cc5
elorenzen <eric.lorenzen@gmail.com>
2019-05-13 12:30:36 -0600

    Use ejs by writing app.set()

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Code to remove need to include '.ejs' after <filename>
+app.set('view engine', 'ejs');
+
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
