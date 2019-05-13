182ca5f9c1356deb84fcec6792b822ff5ed24010
elorenzen <eric.lorenzen@gmail.com>
2019-05-13 13:52:38 -0600

    Added route to /reddit-posts and created redditPosts array for quick data

--- app.js
+++ app.js
@@ -11,6 +11,22 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
 
+app.get('/reddit-posts', (req, res) => {
+    let redditPosts = [
+        {title: `The world now runs on video game logic, what's the first thing you do?`,
+         subredditName: `r/AskReddit`,
+         upvotes: '5.7k'},
+
+        {title: `Exclusive: Amazon rolls out machines that pack orders and replace jobs`,
+         subredditName: `r/news`,
+         upvotes: '476'},
+
+        {title: `If you were a citizen of King's Landing, what kind of ruler would you prefer?`,
+         subredditName: `r/GOT`,
+         upvotes: '1.3k'}
+    ]
+})
+
 app.listen(port, () => {
     console.log(`app.listen on port ${port}`);
 })
