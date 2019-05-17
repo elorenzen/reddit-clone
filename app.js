28ae86c900e37abc622bd414f85704a828f55467
elorenzen <eric.lorenzen@gmail.com>
2019-05-17 11:51:27 -0600

    Updated post route to push new comment from comments/new.ejs into corresponding Reddit Post

--- app.js
+++ app.js
@@ -136,6 +136,31 @@ app.get('/reddit-posts/:id/comments/new', (req, res) => {
             res.render('comments/new', {post: foundPost});
         }
     });
+});
+
+// === CREATE ===
+app.post('/reddit-posts/:id/comments', (req, res) => {
+    // Lookup Reddit post using ID
+    RedditPost.findById(req.params.id, (err, foundPost) => {
+        if(err){
+            console.log(err);
+            res.redirect('/reddit-posts');
+        } else {
+            // Create new comment
+            Comment.create(req.body.comment, (err, comment) => {
+                if(err) {
+                    console.log(err);
+                } else {
+                    // Connect comment to Reddit post
+                    foundPost.comments.push(comment._id);
+                    foundPost.save(); 
+                    
+                    // Redirect to Reddit post with corresponding ID
+                    res.redirect(`/reddit-posts/${foundPost._id}`);
+                }
+            });
+        }
+    });
 })
 
 // =============================================================================
