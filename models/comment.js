c718575afe290c8ce03ebfbbbc6120a8aabc4654
elorenzen <eric.lorenzen@gmail.com>
2019-05-19 14:13:02 -0600

    Added new properties to Comment model

--- models/comment.js
+++ models/comment.js
@@ -2,7 +2,13 @@ const mongoose = require("mongoose");
  
 const commentSchema = new mongoose.Schema({
     text: String,
-    author: String,
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        username: String
+    },
     timeSincePosting: String,
     points: Number,
     replyCount: Number
