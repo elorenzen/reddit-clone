4e22f6825fe0361b81bb18ad430b923a38781846
elorenzen <eric.lorenzen@gmail.com>
2019-05-19 12:23:08 -0600

    Removed isLoggedIn middleware from app.js and put in auth.js

--- app.js
+++ app.js
@@ -170,15 +170,6 @@ post_list.forEach((post) => {
 });
 
 // =============================================================================
-
-function isLoggedIn(req, res, next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    res.redirect('/login');
-}
-
-// =============================================================================
 app.listen(port, () => {
     console.log(`app.listen on port ${port}`);
 })
--- routes/auth.js
+++ routes/auth.js
@@ -43,3 +43,13 @@ app.get('/logout', (req, res) => {
     req.logout();
     res.redirect('/reddit-posts');
 });
+
+
+// =============================================================================
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/login');
+}
