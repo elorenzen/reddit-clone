a87709cacc434f7ef129c8fb77806bc4b39dc260
elorenzen <eric.lorenzen@gmail.com>
2019-05-21 16:49:06 -0600

    Created middleware function checking Reddit post comment author and added middleware to edit, update, and delete routes

--- routes/comments.js
+++ routes/comments.js
@@ -48,7 +48,7 @@ router.post('/', isLoggedIn, (req, res) => {
 });
 
 // === EDIT ===
-router.get('/:comment_id/edit', (req, res) => {
+router.get('/:comment_id/edit', checkCommentOwner, (req, res) => {
     Comment.findById(req.params.comment_id, (err, foundComment) => {
         if(err) {
             res.redirect('back');
@@ -59,7 +59,7 @@ router.get('/:comment_id/edit', (req, res) => {
 })
 
 // === UPDATE ===
-router.put('/:comment_id', (req, res) => {
+router.put('/:comment_id', checkCommentOwner, (req, res) => {
     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment) => {
         if (err) {
             res.redirect('back');
@@ -70,7 +70,7 @@ router.put('/:comment_id', (req, res) => {
 });
 
 // === DELETE ===
-router.delete('/:comment_id', (req, res) => {
+router.delete('/:comment_id', checkCommentOwner, (req, res) => {
     Comment.findByIdAndRemove(req.params.comment_id, (err) => {
         if(err){
             res.redirect('back');
@@ -82,6 +82,27 @@ router.delete('/:comment_id', (req, res) => {
 
 
 // =============================================================================
+
+function checkCommentOwner(req, res, next) {
+    // Check if user is logged in
+    if(req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, (err, foundComment) => {
+            if(err) {
+                res.redirect('back');
+            } else {
+                // If user owns post, allow edits
+                if(foundComment.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect('back');
+                }
+            }
+        });
+    // If user isn't logged in, redirect
+    } else {
+        res.redirect('back');
+    }
+};
 
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
